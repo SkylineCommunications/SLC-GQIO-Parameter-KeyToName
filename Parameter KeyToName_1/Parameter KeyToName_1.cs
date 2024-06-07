019b9f86c9f4d01ae85354f6c7488a91fdeabc49
Pieter Van Compernolle <pieter.van.compernolle@skyline.be>
2024-06-07 19:46:26 +0200

    fix warnings

--- Parameter KeyToName_1/Parameter KeyToName_1.cs
+++ Parameter KeyToName_1/Parameter KeyToName_1.cs
@@ -1,46 +1,43 @@
 using System;
+using System.Collections.Generic;
 using Skyline.DataMiner.Analytics.GenericInterface;
 using Skyline.DataMiner.Net;
 using Skyline.DataMiner.Net.Messages;
-using System.Diagnostics;
-using System.Text;
-using System.Collections.Generic;
-
 
 [GQIMetaData(Name = "Parameter KeyToName")]
 public class ParameterKeyToName : IGQIRowOperator, IGQIInputArguments, IGQIColumnOperator, IGQIOnInit
 {
-    private readonly GQIColumnDropdownArgument _ParameterKeyColumnArg = new GQIColumnDropdownArgument("Parameter Key Column") { IsRequired = true };
-    private readonly GQIColumnDropdownArgument _ProtocolColumnArg = new GQIColumnDropdownArgument("Protocol Name") { IsRequired = false };
-    private readonly GQIColumnDropdownArgument _ProtocolVersionColumnArg = new GQIColumnDropdownArgument("Protocol Version") { IsRequired = false };
-    private readonly GQIStringColumn _ParameterNameColumn = new GQIStringColumn("Parameter Name");
+    private readonly GQIColumnDropdownArgument _parameterKeyColumnArg = new GQIColumnDropdownArgument("Parameter Key Column") { IsRequired = true };
+    private readonly GQIColumnDropdownArgument _protocolColumnArg = new GQIColumnDropdownArgument("Protocol Name") { IsRequired = false };
+    private readonly GQIColumnDropdownArgument _protocolVersionColumnArg = new GQIColumnDropdownArgument("Protocol Version") { IsRequired = false };
+    private readonly GQIStringColumn _parameterNameColumn = new GQIStringColumn("Parameter Name");
+
+    private IConnection _conn;
 
     private GQIColumn _parameterKeyColumn;
     private GQIColumn _protocolColumn;
     private GQIColumn _protocolVersionColumn;
-    private Exception _LastException;
 
     private GQIDMS _dms;
-    private IConnection _Conn;
     private ProtocolCache _protocolCache = new ProtocolCache();
 
     public GQIArgument[] GetInputArguments()
     {
-        return new GQIArgument[] { _ParameterKeyColumnArg, _ProtocolColumnArg, _ProtocolVersionColumnArg };
+        return new GQIArgument[] { _parameterKeyColumnArg, _protocolColumnArg, _protocolVersionColumnArg };
     }
 
     public OnArgumentsProcessedOutputArgs OnArgumentsProcessed(OnArgumentsProcessedInputArgs args)
     {
-        _parameterKeyColumn = args.GetArgumentValue(_ParameterKeyColumnArg);
-        _protocolColumn = args.GetArgumentValue(_ProtocolColumnArg);
-        _protocolVersionColumn = args.GetArgumentValue(_ProtocolVersionColumnArg);
+        _parameterKeyColumn = args.GetArgumentValue(_parameterKeyColumnArg);
+        _protocolColumn = args.GetArgumentValue(_protocolColumnArg);
+        _protocolVersionColumn = args.GetArgumentValue(_protocolVersionColumnArg);
 
         return new OnArgumentsProcessedOutputArgs();
     }
 
     public void HandleColumns(GQIEditableHeader header)
     {
-        header.AddColumns(_ParameterNameColumn);
+        header.AddColumns(_parameterNameColumn);
     }
 
     public OnInitOutputArgs OnInit(OnInitInputArgs args)
@@ -50,13 +47,11 @@ public class ParameterKeyToName : IGQIRowOperator, IGQIInputArguments, IGQIColum
             _dms = args.DMS;
             if (_dms != null)
             {
-                //_Conn = _dms.GetConnection();
+                _conn = _dms.GetConnection();
             }
-
         }
-        catch (Exception ex)
+        catch (Exception)
         {
-            _LastException = ex;
         }
 
         return new OnInitOutputArgs();
@@ -66,63 +61,66 @@ public class ParameterKeyToName : IGQIRowOperator, IGQIInputArguments, IGQIColum
     {
         try
         {
-            //Init name with Key
+            // Init name with Key
             String parameterKey = row.GetValue<String>(_parameterKeyColumn);
             ParamID paramID = ParamID.FromString(parameterKey);
 
             // Find the correct protocol, based on the columns that have been linked this can be either a raw protocol (better performance/caching) or an element protocol
             GetProtocolInfoResponseMessage protocolInfo = null;
             if (_protocolColumn != null && _protocolVersionColumn != null)
-            {//by protocol				
+            {// by protocol
                 String protocol = row.GetValue<String>(_protocolColumn);
                 String version = row.GetValue<String>(_protocolVersionColumn);
 
-                if (!_protocolCache.tryGet($"{protocol}/{version}", out protocolInfo))
+                if (!_protocolCache.TryGet($"{protocol}/{version}", out protocolInfo))
                 {
-                    protocolInfo = _Conn.GetProtocol(protocol, version);
+                    protocolInfo = _conn.GetProtocol(protocol, version);
                     _protocolCache.Cache($"{protocol}/{version}", protocolInfo);
                 }
             }
-            else if (!_protocolCache.tryGet($"{paramID.DataMinerID}/{paramID.EID}", out protocolInfo))
-            {//by element
-                protocolInfo = _Conn.GetElementProtocol(paramID.DataMinerID, paramID.EID);
+            else if (!_protocolCache.TryGet($"{paramID.DataMinerID}/{paramID.EID}", out protocolInfo))
+            {// by element
+                protocolInfo = _conn.GetElementProtocol(paramID.DataMinerID, paramID.EID);
                 _protocolCache.Cache($"{paramID.DataMinerID}/{paramID.EID}", protocolInfo);
             }
 
-            //Once we have the protocol, lookup the map IDToName
+            // Once we have the protocol, lookup the map IDToName
             String parameterName = parameterKey;
             if (protocolInfo != null)
             {
                 parameterName = protocolInfo.GetParameterName(paramID.PID);
             }
 
-            row.SetValue<String>(_ParameterNameColumn, parameterName);
-
+            row.SetValue<String>(_parameterNameColumn, parameterName);
         }
-        catch (Exception ex) { row.SetValue<String>(_ParameterNameColumn, "Some parameter name"); }
+        catch (Exception)
+		{
+			row.SetValue<String>(_parameterNameColumn, "Some parameter name");
+		}
     }
 }
 
 public class ProtocolCache
 {
-    private Dictionary<String, GetProtocolInfoResponseMessage> _Map = new Dictionary<String, GetProtocolInfoResponseMessage>();
+    private readonly Dictionary<String, GetProtocolInfoResponseMessage> _map = new Dictionary<String, GetProtocolInfoResponseMessage>();
 
     public ProtocolCache()
-    { }
+	{
+	}
 
     public void Cache(String key, GetProtocolInfoResponseMessage protocol)
     {
-        lock (_Map)
+        lock (_map)
         {
-            _Map[key] = protocol;
+            _map[key] = protocol;
         }
     }
 
-    public bool tryGet(String key, out GetProtocolInfoResponseMessage protocol)
+    public bool TryGet(String key, out GetProtocolInfoResponseMessage protocol)
     {
-        lock (_Map)
+        lock (_map)
         {
-            return _Map.TryGetValue(key, out protocol);
+            return _map.TryGetValue(key, out protocol);
         }
     }
 }
